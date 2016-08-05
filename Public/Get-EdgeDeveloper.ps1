e1e6441a793d4f1cb4cbd7dbb9170aa0230038d1
Dino Chiesa <dchiesa@apigee.com>
2016-08-05 15:04:05 -0700

    add Get-EdgeDeveloper

---
+++ Public/Get-EdgeDeveloper.ps1
@@ -0,0 +1,49 @@
+Function Get-EdgeDeveloper {
+    <#
+    .SYNOPSIS
+        Get one or more developers from Apigee Edge
+
+    .DESCRIPTION
+        Get one or more developers from Apigee Edge
+
+    .PARAMETER Org
+        The Apigee Edge organization. 
+
+    .PARAMETER Params
+        Hash table with query options for the specific collection type
+
+        Example for getting all details of developers:
+            -Params @{
+                expand  = 'true'
+            }
+
+    .EXAMPLE
+        Get-EdgeDeveloper -Org cap500
+
+    .EXAMPLE
+        Get-EdgeDeveloper -Params @{ expand = 'true' }
+
+    .FUNCTIONALITY
+        ApigeeEdge
+
+    #>
+
+    [cmdletbinding()]
+    param(
+        [string]$Org,
+        [Hashtable]$Params
+    )
+    
+    $Options = @{
+        Collection = 'developers'
+    }
+    
+    if ($PSBoundParameters['Debug']) {
+        $Options.Add( 'Debug', $Debug )
+    }
+    if ($PSBoundParameters['Params']) {
+        $Options.Add( 'Params', $Params )
+    }
+
+    Get-EdgeObject $Options
+}
