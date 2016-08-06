f3d169e930142568de62c83a9ba041f1729b15ff
Dino Chiesa <dchiesa@apigee.com>
2016-08-05 18:10:44 -0700

    add Get-EdgeEnvironment

---
+++ Public/Get-EdgeEnvironment.ps1
@@ -0,0 +1,53 @@
+Function Get-EdgeEnvironment {
+    <#
+    .SYNOPSIS
+        Get one or more environments from Apigee Edge
+
+    .DESCRIPTION
+        Get one or more environments from Apigee Edge
+
+    .PARAMETER Name
+        The name of the environment to retrieve.
+        The default is to list all environments.
+
+    .PARAMETER Org
+        The Apigee Edge organization. The default is to use the value from Set-EdgeConnection.
+
+    .EXAMPLE
+        Get-EdgeEnvironment -Org cap500
+
+    .EXAMPLE
+        Get-EdgeEnvironment -Org cap500 -Name test
+
+    .FUNCTIONALITY
+        ApigeeEdge
+
+    #>
+
+    [cmdletbinding()]
+    param(
+        [string]$Name,
+        [string]$Org,
+        [Hashtable]$Params
+    )
+    
+    if ($PSBoundParameters['Debug']) {
+        $DebugPreference = 'Continue'
+    }
+    
+    $Options = @{
+        Collection = 'environments'
+    }
+    
+    if ($PSBoundParameters['Debug']) {
+        $Options.Add( 'Debug', $Debug )
+    }
+    
+    if ($PSBoundParameters['Name']) {
+        $Options.Add( 'Name', $Name )
+    }
+
+    Write-Debug ( "Options @Options`n" )
+
+    Get-EdgeObject @Options
+}
