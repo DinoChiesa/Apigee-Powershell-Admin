ca99f773b18d4aa09740ccb161cdc6a9f36f6a45
Dino Chiesa <dchiesa@apigee.com>
2016-08-19 20:08:51 -0700

    fix incorrect path in delete-edgedevapp

--- PSApigeeEdge/Public/Delete-EdgeDevApp.ps1
+++ PSApigeeEdge/Public/Delete-EdgeDevApp.ps1
@@ -46,7 +46,7 @@ Function Delete-EdgeDevApp {
         throw [System.ArgumentNullException] 'use -Name and -Developer.'
     }
 
-    $Options.Add( 'Collection', $( Join-Parts -Separator '/' -Parts 'developers', $Developer, 'app' ))
+    $Options.Add( 'Collection', $( Join-Parts -Separator '/' -Parts 'developers', $Developer, 'apps'))
     $Options.Add( 'Name', $Name)
 
     if ($PSBoundParameters['Debug']) {
