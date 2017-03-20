b5ed921a6fc169b8ad98809513cdf584eb037007
Dino Chiesa <dpchiesa@hotmail.com>
2017-03-20 11:49:28 -0700

    add first example

---
+++ Examples/Find-ApiProductForProxy.ps1
@@ -0,0 +1,26 @@
+# Import-Module $PSScriptRoot\..\PSApigeeEdge -Force
+Write-Output "Find API Product for a given API Proxy"
+
+$Org = Read-Host "Edge org?"
+$User = Read-Host "Edge admin user?"  # eg, 'dchiesa@google.com'
+$SecurePass = Read-Host -assecurestring "Password"
+$EncryptedPassword = ConvertFrom-SecureString $SecurePass
+
+$connection = @{
+   Org = $Org
+   User = $User
+   EncryptedPassword = $EncryptedPassword
+}
+
+Set-EdgeConnection @connection
+
+$proxyToFind = Read-Host "Proxy to find"
+
+$prods = @(Get-EdgeApiProduct -Params @{ expand = 'true' }).apiProduct
+Write-Output ("Total Products: " + $prods.count)
+
+$filteredProds = $prods |? { $_.proxies -contains $proxyToFind }
+
+Write-Output ([string]::Format('Products: {0}', (ConvertTo-Json $filteredProds) ) )
+
+
