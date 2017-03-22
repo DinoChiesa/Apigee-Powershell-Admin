87f29263fed6e2bf0a807adea702c5f53de48e96
Dino Chiesa <dpchiesa@hotmail.com>
2017-03-22 15:12:28 -0700

    allow Find-EdgeApp to accept the Org parameter

--- PSApigeeEdge/Public/Find-EdgeApp.ps1
+++ PSApigeeEdge/Public/Find-EdgeApp.ps1
@@ -1,20 +1,20 @@
 Function Find-EdgeApp {
     <#
     .SYNOPSIS
-        Finds an Edge App given the API Key. 
+        Finds an Edge App given the API Key.
 
     .DESCRIPTION
-        Finds an Edge App given the API Key. The result is the developer app that 
+        Finds an Edge App given the API Key. The result is the developer app that
         owns the credential with that API key.
 
     .PARAMETER ConsumerKey
-        The API key to find. 
+        Required. The API key to find.
 
     .PARAMETER Org
-        The Apigee Edge organization. The default is to use the value from Set-EdgeConnection.
+        Optional. The Apigee Edge organization. The default is to use the value from Set-EdgeConnection.
 
     .EXAMPLE
-        Find-EdgeApp -ConsumerKey B792a022098d48618c6d 
+        Find-EdgeApp -ConsumerKey B792a022098d48618c6d
 
     .LINK
         Add-EdgeAppCredential
@@ -33,23 +33,25 @@ Function Find-EdgeApp {
         [string]$Org
     )
 
-
     $Options = @{ }
 
     if ($PSBoundParameters['Debug']) {
         $Options.Add( 'Debug', $Debug )
+    }
+    if ($PSBoundParameters['Org']) {
+        $Options.Add( 'Org', $Org )
     }
 
     if (!$PSBoundParameters['ConsumerKey']) {
         throw [System.ArgumentNullException] "ConsumerKey", "You must specify the -ConsumerKey option."
     }
 
-    # $appsExpanded = @(Get-EdgeDevApp -Params @{ expand = 'true' }).app
-    
-    $theApp = @( @( Get-EdgeDevApp -Params @{ expand = 'true' }).app |? {
+    $Options.Add( 'Params', @{ expand = 'true' })
+
+    $theApp = @( @( Get-EdgeDevApp @Options ).app |? {
                      $_.credentials |? { $_.consumerKey -eq $ConsumerKey }
                  })
-    
+
     if ($theApp.count -gt 1) {
         throw [System.SystemException] "More than one app was found with that key."
     }
