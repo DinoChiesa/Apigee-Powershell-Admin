2ab5cf977ede06a34b2ee862c5c66ff35fb00ccc
Dino Chiesa <dpchiesa@hotmail.com>
2017-03-22 15:44:26 -0700

    rename Update-EdgeAppCredentialStatus to Update-EdgeDevAppStatus

--- PSApigeeEdge/Public/Update-EdgeDevAppStatus.ps1
+++ PSApigeeEdge/Public/Update-EdgeDevAppStatus.ps1
@@ -1,13 +1,14 @@
-Function Update-EdgeAppCredentialStatus {
+Function Update-EdgeDevAppStatus {
     <#
     .SYNOPSIS
-        Approve or Revoke an existing credential, or an apiproduct on a credential, for a developer app.
+        Approve or Revoke an existing credential, or an apiproduct on a credential, for a developer app. Or the entire app.
 
     .DESCRIPTION
         Approve or Revoke an existing credential from a developer app. Or, approve or revoke a specific
-        API Product on a credential for a developer app. Approving a credential that is pending
-        or has been revoked allows the credential to be used at runtime. Revoking a credential means that
-        the key will be rejected at runtime.
+        API Product on a credential for a developer app. Approving an {app, credential, product} that is pending
+        or has been revoked allows the {app, credential, product} to be used at runtime. Revoking same means that
+        the key will be rejected at runtime. Note that the entire chain {app, credential, product } must have
+        status=approved for the key to be accepted.
 
     .PARAMETER AppName
         Required. The name of the developer app that contains the credential to be updated.
@@ -15,29 +16,40 @@ Function Update-EdgeAppCredentialStatus {
     .PARAMETER Developer
         Required. The id or email of the developer that owns the app that contains the credential.
 
-    .PARAMETER Key
-        Required. The consumer key for the credential to be approved or revoked.
-
     .PARAMETER Action
         Required. The action to apply. Either revoke or approve.
 
+    .PARAMETER Key
+        Optional. The consumer key for the credential to be approved or revoked. If omitted, the Action
+        applies to the app.
+
     .PARAMETER ApiProduct
         Optional. The name of the API Product to be approved or revoked. It must be present on the credential.
+        This is ignored if the Key is not also passed.
 
     .PARAMETER Org
         Optional. The Apigee Edge organization. The default is to use the value from Set-EdgeConnection.
 
     .EXAMPLE
-        Update-EdgeAppCredentialStatus -AppName DPC6 -Developer dchiesa@example.org -Key pd0mg1FuedmfCpY9gWZonQmR2fGD3Osw -Action revoke
+        Update-EdgeDevAppStatus -AppName DPC6 -Developer dchiesa@example.org -Key pd0mg1FuedmfCpY9gWZonQmR2fGD3Osw -Action revoke
 
     .EXAMPLE
-        Update-EdgeAppCredentialStatus -AppName DPC6 -Developer dchiesa@example.org -Key pd0mg1FuedmfCpY9gWZonQmR2fGD3Osw -ApiProduct Product123 -Action approve
+        Update-EdgeDevAppStatus -AppName DPC6 -Developer dchiesa@example.org -Key pd0mg1FuedmfCpY9gWZonQmR2fGD3Osw -ApiProduct Product123 -Action approve
+
+    .EXAMPLE
+        Update-EdgeDevAppStatus -AppName DPC6 -Developer dchiesa@example.org -Action approve
 
     .LINK
         Revoke-EdgeAppCredential
 
     .LINK
         Approve-EdgeAppCredential
+
+    .LINK
+        Approve-EdgeDevApp
+
+    .LINK
+        Revoke-EdgeDevApp
 
     .FUNCTIONALITY
         ApigeeEdge
@@ -70,9 +82,6 @@ Function Update-EdgeAppCredentialStatus {
     if (!$PSBoundParameters['AppName']) {
       throw [System.ArgumentNullException] "AppName", "You must specify the -AppName option."
     }
-    if (!$PSBoundParameters['Key']) {
-      throw [System.ArgumentNullException] "Key", "You must specify the -Key option."
-    }
     if (!$PSBoundParameters['Action']) {
       throw [System.ArgumentNullException] "Action", "You must specify the -Action option."
     }
@@ -81,13 +90,20 @@ Function Update-EdgeAppCredentialStatus {
       throw [System.ArgumentException] "Action", "Action must be 'revoke' or 'approve'."
     }
 
-    if ($PSBoundParameters['ApiProduct']) {
-        $Options.Add( 'Collection', $(Join-Parts -Separator '/' -Parts 'developers', $Developer, 'apps', $AppName, 'keys', $Key, 'apiproducts' ) )
-        $Options.Add( 'Name', $ApiProduct )
+    if ($PSBoundParameters['Key']) {
+        if ($PSBoundParameters['ApiProduct']) {
+            $Options.Add( 'Collection', $(Join-Parts -Separator '/' -Parts 'developers', $Developer, 'apps', $AppName, 'keys', $Key, 'apiproducts' ) )
+            $Options.Add( 'Name', $ApiProduct )
+        }
+        else {
+            $Options.Add( 'Collection', $(Join-Parts -Separator '/' -Parts 'developers', $Developer, 'apps', $AppName, 'keys' ) )
+            $Options.Add( 'Name', $Key )
+        }
     }
     else {
-        $Options.Add( 'Collection', $(Join-Parts -Separator '/' -Parts 'developers', $Developer, 'apps', $AppName, 'keys' ) )
-        $Options.Add( 'Name', $Key )
+        # Just update the app status
+        $Options.Add( 'Collection', $(Join-Parts -Separator '/' -Parts 'developers', $Developer, 'apps' ) )
+        $Options.Add( 'Name', $AppName )
     }
     $Options.Add( 'QParams', $( ConvertFrom-HashtableToQueryString @{ action = $Action } ))
 
