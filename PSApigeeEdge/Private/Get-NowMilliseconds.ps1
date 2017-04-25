e066a139ba16287e8ec98e3dc04138339c67127b
Dino Chiesa <dpchiesa@hotmail.com>
2017-04-25 15:29:24 -0700

    update Get-NowMilliseconds

--- PSApigeeEdge/Private/Get-NowMilliseconds.ps1
+++ PSApigeeEdge/Private/Get-NowMilliseconds.ps1
@@ -1,9 +1,19 @@
 Function Get-NowMilliseconds
 {
     PARAM([int]$Fudge=0)
-    if ($Fudge -and $Fudge -ne 0) {
-        $Now = [DateTime]::UtcNow.AddMilliseconds($Fudge)
+    PROCESS {
+        if ($PSBoundParameters['Debug']) {
+            $DebugPreference = 'Continue'
+        }
+        $Now = if ($Fudge -and $Fudge -ne 0) {
+            [DateTime]::UtcNow.AddMilliseconds($Fudge)
+        }
+        else {
+            [DateTime]::UtcNow
+        }
+        $UnixEpochStart = new-object DateTime 1970,1,1,0,0,0,([DateTimeKind]::Utc)
+        $NowMilliseconds = [int64](New-TimeSpan -Start $UnixEpochStart -End $Now).TotalMilliseconds
+        write-debug ("NowMilliseconds: " + $NowMilliseconds)
+        $NowMilliseconds
     }
-    $UnixEpochStart = new-object DateTime 1970,1,1,0,0,0,([DateTimeKind]::Utc)
-    $NowMilliseconds = [int64](New-TimeSpan -Start $UnixEpochStart -End $Now).TotalMilliseconds
 }
